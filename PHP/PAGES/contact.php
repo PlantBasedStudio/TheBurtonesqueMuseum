84b2c4117cf92f0a419cc86190092bc035bb4746
Alexy <delplace.alexis02@gmail.com>
2023-07-13 11:17:41 +0200

    M A J pour la page contact

--- PHP/PAGES/contact.php
+++ PHP/PAGES/contact.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include '../COMPONENTS/important_link.php' ?>
+    <link rel="stylesheet" href="../../CSS/contact_style.css">
     <title>Contact</title>
 </head>
-<body>
-<?php include '../COMPONENTS/header.php' ?>
 
-<!-- Ton code ici alexy :D -->
-<?php include '../COMPONENTS/footer.php' ?>
+<body>
+    <?php include '../COMPONENTS/header.php' ?>
+
+    <h1>CONTACT</h1>
+    <form class="container">
+
+        <input type="email" id="email" name="email" required placeholder="Votre adresse e-mail ...">
+
+        <input type="text" id="object" name="object" pattern="[0-9]{2}" required placeholder="Objet de votre demande ...">
+
+        <textarea id="message" name="message" rows="4" cols="35" required placeholder="Votre message ..."></textarea>
+        <input type="submit" value="Envoyer" class="submit">
+    </form>
+
+    <?php include '../COMPONENTS/footer.php' ?>
 </body>
+
 </html>
