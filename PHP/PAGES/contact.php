fc5a76b16061b0014a7d2043f002f47594ef0b81
Damien <83568840+PlantBasedStudio@users.noreply.github.com>
2023-07-13 11:40:13 +0000

    Review

--- PHP/PAGES/contact.php
+++ PHP/PAGES/contact.php
@@ -12,17 +12,7 @@
 <body>
     <?php include '../COMPONENTS/header.php' ?>
 
-    <h1>CONTACT</h1>
-    <form class="container">
-
-        <input type="email" id="email" name="email" required placeholder="Votre adresse e-mail ...">
-
-        <input type="text" id="object" name="object" pattern="[0-9]{2}" required placeholder="Objet de votre demande ...">
-
-        <textarea id="message" name="message" rows="4" cols="35" required placeholder="Votre message ..."></textarea>
-        <input type="submit" value="Envoyer" class="submit">
-    </form>
-
+    <?php include '../COMPONENTS/contact_component.php' ?>
     <?php include '../COMPONENTS/footer.php' ?>
 </body>
 
