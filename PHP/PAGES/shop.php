6d95ce6e00732c1a83d2433adfe5354841a7b2f7
Alexy <delplace.alexis02@gmail.com>
2023-07-20 08:55:31 +0200

    Mis à jour du Shop

--- PHP/PAGES/shop.php
+++ PHP/PAGES/shop.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include '../COMPONENTS/important_link.php' ?>
-    <link rel="stylesheet" href="../../CSS/shop_style.css">
+    <link rel="stylesheet" href="../../CSS/shop_stylee.css">
 
     <title>Boutique</title>
 </head>
